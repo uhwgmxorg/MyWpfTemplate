f9063caa840ebffdbd7fee2816e06e8a75ba192a
Uwe Walter <uhw@gmx.org>
2020-03-12 13:54:47 +0100

    Start version 0.0.0.0.

---
+++ MyWpfTemplate/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+
+namespace MyWpfTemplate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Construktor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /******************************/
+        /*       Button Events        */
+        /******************************/
+        #region Button Events
+
+        #endregion
+        /******************************/
+        /*      Menu Events          */
+        /******************************/
+        #region Menu Events
+
+        #endregion
+        /******************************/
+        /*      Other Events          */
+        /******************************/
+        #region Other Events
+
+        #endregion
+        /******************************/
+        /*      Other Functions       */
+        /******************************/
+        #region Other Functions
+
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# MyWpfTemplate
+Just a private WPF Template
+
+![img](https://github.com/uhwgmxorg/MyWpfTemplate/blob/master/1.PNG)
+Version 0.0.0.0
