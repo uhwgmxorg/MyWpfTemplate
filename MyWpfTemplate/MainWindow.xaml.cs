a00794db6a3193eb6b8757eb9fe283714056ad3d
Uwe Walter <uhw@gmx.org>
2020-03-12 14:11:09 +0100

    Add buttons and prepare for bindings, version 0.1.0.0.

--- MyWpfTemplate/MainWindow.xaml.cs
+++ MyWpfTemplate/MainWindow.xaml.cs
@@ -1,15 +1,16 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
 
 namespace MyWpfTemplate
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        /// <summary>
-        /// Construktor
-        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -19,6 +20,21 @@ namespace MyWpfTemplate
         /*       Button Events        */
         /******************************/
         #region Button Events
+
+        private void Button_1_Click(object sender, RoutedEventArgs e)
+        {
+            System.Console.Beep();
+        }
+
+        /// <summary>
+        /// Button_Close_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
 
         #endregion
         /******************************/
@@ -37,6 +53,27 @@ namespace MyWpfTemplate
         /*      Other Functions       */
         /******************************/
         #region Other Functions
+
+        /// <summary>
+        /// SetField
+        /// for INotify Changed Properties
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <param name="value"></param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        protected bool SetField<T>(ref T field, T value, string propertyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        private void OnPropertyChanged(string p)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(p));
+        }
 
         #endregion
     }
--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 Just a private WPF Template
 
 ![img](https://github.com/uhwgmxorg/MyWpfTemplate/blob/master/1.PNG)
+
 Version 0.0.0.0
+
+![img](https://github.com/uhwgmxorg/MyWpfTemplate/blob/master/2.PNG)
+
+Version 0.1.0.0
+Add buttons and prepare for bindings.
